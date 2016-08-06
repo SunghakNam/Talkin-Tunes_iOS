3ed1fa18fb46cd1f197fc67cc3c51397db7caed5
Sunghak Nam <shnam@sunghaks-macbook-pro.local>
2016-08-06 23:57:05 +0900

    Initial Commit

---
+++ Alpha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alpha
+//
+//  Created by Sunghak Nam on 8/6/16.
+//  Copyright © 2016 Sunghak Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
